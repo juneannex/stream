4693960689449f9808c0b583579436f945e94012
juneannex <deargod1986@gmail.com>
2024-06-12 13:01:41 +0800

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -13,17 +13,6 @@ def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
         #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-    end = response.find('.m3u8') + 5
     tuner = 100
     while True:
         if 'https://' in response[end-tuner : end]:
